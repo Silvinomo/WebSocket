0b0d1ef6b74057b34c6b0d9f38f79f8410c83b36
SilvinoMo <silvino_miranda@hotmail.com>
2021-06-12 00:06:31 -0300

    Update

--- WebsocketNodeServer/index.js
+++ WebsocketNodeServer/index.js
@@ -3,31 +3,29 @@ var socket = require('socket.io');
 
 // App setup
 var app = express();
-var server = app.listen(3000, function(){
-    console.log('listening for requests on port 3000');
+var server = app.listen(3000, function () {
+  console.log('listening for requests on port 3000');
 });
 
 // Socket setup
 var io = socket(server);
 
-// Listen for new connection and print a message in console 
-io.on('connection', (socket) => {
+// Listen for new connection and print a message in console
+io.on('connection', (socketIO) => {
+  console.log(`New connection ${socketIO.id}`);
 
-    console.log(`New connection ${socket.id}`)
+  // Listening for chat event
+  socketIO.on('chat', function (data) {
+    // console.log('chat event trigged at server');
+    // console.log('need to notify all the clients about this event');
+    io.sockets.emit('chat', data);
+  });
 
-    // Listening for chat event
-    socket.on('chat', function(data){
-        // console.log('chat event trigged at server');
-        // console.log('need to notify all the clients about this event');
-        io.sockets.emit('chat', data);
-    });
-
-    // Listening for typing event
-    socket.on('typing', function(data){
-        // console.log(`Server received ${data} is typing`);
-        // console.log('need to inform all the clients about this');
-        io.sockets.emit('typing', data);
-        //socket.broadcast.emit('typing', data);
-    });
-
+  // Listening for typing event
+  socketIO.on('typing', function (data) {
+    // console.log(`Server received ${data} is typing`);
+    // console.log('need to inform all the clients about this');
+    io.sockets.emit('typing', data);
+    //socketIO.broadcast.emit('typing', data);
+  });
 });
